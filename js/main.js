9c509a9d9b00084206dcf7aea0c7849c8fe88af9
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-04-04 15:53:41 +0500

    Add main.js

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const openMenu = document.querySelector('.openMenu');
+const closeMenu = document.querySelector('.closeMenu');
+function openMobileMenu() {
+	mainMenu.style.display = 'flex';
+  	mainMenu.style.top = '0';
+}
+function closeMobileMenu() {
+	mainMenu.style.top = '-100%';
+}
+openMenu.addEventListener('click', openMobileMenu);
+closeMenu.addEventListener('click', closeMobileMenu);
