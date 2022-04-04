48e47dc40a3ea9cddaeaf7df612040ae96efa27d
jodrack <drack.sir01@gmail.com>
2022-04-04 14:12:29 +0200

    Fix a slight javascript variable issue

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
+const mainMenu = document.querySelector('.mainMenu');
 const openMenu = document.querySelector('.openMenu');
 const closeMenu = document.querySelector('.closeMenu');
+
 function openMobileMenu() {
-	mainMenu.style.display = 'flex';
-  	mainMenu.style.top = '0';
+  mainMenu.style.display = 'flex';
+  mainMenu.style.top = '0';
 }
+
 function closeMobileMenu() {
-	mainMenu.style.top = '-100%';
+  mainMenu.style.top = '-100%';
 }
+
 openMenu.addEventListener('click', openMobileMenu);
 closeMenu.addEventListener('click', closeMobileMenu);
